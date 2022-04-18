b3e0082bd62cb1332bb5e174ba24407769161887
maheshpapolu <maheshnaidu9666@gmail.com>
2022-04-18 23:17:44 +0530

    [Mahesh]Find_The_Maximum

--- Generics/FindMaximum.cs
+++ Generics/FindMaximum.cs
@@ -8,42 +8,24 @@ namespace Generics
 {
     internal class FindMaximum
     {
-        internal class FindMaximum<T> where T : IComparable // Creating a generic class
+        public static int FindMaxInteger(int first, int second, int third) // Class Method to find maximum of three integer number
         {
-            public static T FindMax(T first, T second, T third)// Creating generic Method to find maximum of three having any data type
+            //Checking that first number is maximum or not
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 || first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 || first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
             {
-                //Checking that first is maximum or not
-                if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 || first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 || first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
-                {
-                    return first; // Retrun first as maximum
-                }
-                //Checking that second is maximum or not
-                if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 || second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 || second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
-                {
-                    return second; // Retrun second as maximum
-                }
-                //Checking that third is maximum or not
-                if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 || third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 || third.CompareTo(first) > 0 && third.CompareTo(second) >= 0)
-                {
-                    return third; // Retrun third as maximum
-                }
-                return first; // Return first if all values are same 
+                return first; // Retrun first as maximum
             }
-
-            public override bool Equals(object obj)
+            //Checking that second number is maximum or not
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 || second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 || second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
             {
-                return base.Equals(obj);
+                return second; // Retrun second as maximum
             }
-
-            public override int GetHashCode()
+            //Checking that third number is maximum or not
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 || third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 || third.CompareTo(first) > 0 && third.CompareTo(second) >= 0)
             {
-                return base.GetHashCode();
+                return third; // Retrun third as maximum
             }
-
-            public override string ToString()
-            {
-                return base.ToString();
-            }
+            return first; // Return first if all the integer are same 
         }
     }
 }
